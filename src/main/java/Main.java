8e3bbc495536adca94d41eb9f309d0845a215a74
artrayme <artrayme@protonmail.com>
2021-11-06 00:35:27 +0300

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import model.DefaultFloatMatrix;
+import model.FastFloatMatrix;
+import model.FloatMatrix;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * @author artrayme
+ * 11/5/21
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+    }
+
+}
+
+
