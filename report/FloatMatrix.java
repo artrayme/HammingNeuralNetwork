3671b0b5db9b669fa1811ff1aa4229f581541b02
artrayme <artrayme@protonmail.com>
2021-11-09 19:07:54 +0300

    add report

---
+++ report/FloatMatrix.java
@@ -0,0 +1,32 @@
+package model;
+
+/**
+ * @author artrayme
+ * 11/5/21
+ */
+public interface FloatMatrix {
+    FloatMatrix mult(FloatMatrix otherMatrix);
+
+    FloatMatrix plusThis(FloatMatrix otherMatrix);
+    FloatMatrix plusThis(float scalar);
+
+    FloatMatrix minus(FloatMatrix otherMatrix);
+
+    FloatMatrix abs();
+
+    FloatMatrix absThis();
+
+    double sum();
+
+    FloatMatrix scale(float scale);
+
+    FloatMatrix scaleThis(float scale);
+
+    FloatMatrix transpose();
+
+    int getHeight();
+
+    int getWidth();
+
+    float[][] toArray();
+}
